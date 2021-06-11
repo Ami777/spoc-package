4b340a48209ef775cb4cae2b6a15201d251dde93
Jakub <jk@itdotfocus.com>
2021-06-11 09:36:05 +0200

    Added JSDoc

--- src/calc.js
+++ src/calc.js
@@ -1,7 +1,28 @@
+/**
+ * Adds two numbers.
+ * ```
+ * const sum = add(2, 3); //5
+ * ```
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 const add = (a, b) => a + b;
 
+/**
+ * Subtracts two numbers.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 const sub = (a, b) => a - b;
 
+/**
+ * Multiplies two numbers.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns number
+ */
 const multi = (a, b) => a * b;
 
 module.exports = {
